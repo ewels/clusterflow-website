32edba1eb16abedcd6e7f8b5b9861fee9191bcab
Phil Ewels <phil.ewels@scilifelab.se>
2017-03-17 09:39:33 +0100

    Merge - use local DB to store version check stats instead of google analytics

--- public_html/version.php
+++ public_html/version.php
@@ -1,5 +1,20 @@
 0.4
 <?php
-require_once('ss-ga.class.php');
-ssga_track( 'UA-69834359-1', 'clusterflow.io', $_SERVER['REQUEST_URI']);
-?>
+
+// Log version of tool that's asking
+$remote_version = trim($_GET['v']);
+$dev = substr($remote_version, -3) == 'dev' ? 'dev' : '';
+$remote_version = preg_replace('/[^\d\.]/', '', $remote_version).$dev;
+
+// Connect to the database
+$dbconfig = parse_ini_file("../config.ini");
+$db = new mysqli('localhost', $dbconfig['user'], $dbconfig['password'], $dbconfig['db']);
+if($db->connect_errno == 0){
+
+    // Insert new record with querying version
+    $stmt = $db->prepare("INSERT INTO version_checks (version, ip, addr) VALUES (?, ?, ?)");
+    $stmt->bind_param('sss',$remote_version, $_SERVER['REMOTE_ADDR'], gethostbyaddr($_SERVER['REMOTE_ADDR']));
+    $stmt->execute();
+    $db->close();
+
+}
